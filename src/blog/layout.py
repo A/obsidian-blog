5436a19fafc2e3f207e42ec6e920e0210b1eded5
Anton Shuvalov <anton@shuvalov.info>
2022-02-06 16:06:29 +0700

    Add layout + test

---
+++ src/blog/layout.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+import os
+from src import fs
+from src.converters import handlebars
+
+
+@dataclass
+class Layout:
+    def __init__(self, filename):
+        self.filename = filename
+        with open(filename) as f:
+            hbs_str = f.read()
+        self.template_fn = handlebars.create_template_fn(hbs_str)
+
+    @property
+    def name(self):
+        name, _ = os.path.splitext(os.path.basename(self.filename))
+        return name
+
+    @property
+    def fn(self):
+        with open(self.filename) as f:
+            hbs_str = f.read()
+        return handlebars.create_template_fn(hbs_str)
+
+    @staticmethod
+    def get_all(layouts_dir):
+        layouts = {}
+
+        for file in fs.get_files_in_dir(layouts_dir):
+            path = os.path.join(layouts_dir, file)
+            layout = Layout(path)
+            layouts[layout.name] = layout
+
+        return layouts
+
+    def render(self, ctx):
+        return self.fn(ctx)
---
+++ tests/blog/layout_test.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+from src.blog.layout import Layout
+from tests.helpers import get_fixture_path
+
+
+def test_layout_parsing():
+    cwd = os.getcwd()
+    fixture_path = get_fixture_path('layouts')
+    os.chdir(fixture_path)
+
+    layouts = Layout.get_all(fixture_path)
+
+    assert len(layouts.items()) == 3
+
+    os.chdir(cwd)
+
+
+@pytest.mark.parametrize(
+    'layout_name',
+    [
+        ('index'),
+        ('main'),
+        ('another'),
+    ],
+)
+def test_layout_rendering(layout_name):
+    cwd = os.getcwd()
+    fixture_path = get_fixture_path('layouts')
+    os.chdir(fixture_path)
+
+    data = 'Hello World'
+    context = {'data': data}
+
+    layouts = Layout.get_all(fixture_path)
+    layout = layouts[layout_name]
+    res = layout.render(context)
+
+    assert res == f'<h1>{layout_name}</h1>\n<pre>{data}</pre>\n'
+
+    os.chdir(cwd)
