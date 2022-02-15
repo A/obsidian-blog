e2d315e1e4a5cd30e3b37121d4c5e7461ba4aa9e
Anton Shuvalov <anton@shuvalov.info>
2022-02-15 20:45:55 +0700

    More further_reading tests

--- tests/preprocessors/further_reading_test.py
+++ tests/preprocessors/further_reading_test.py
@@ -22,3 +22,44 @@ def test_further_reading_preprocessor(snapshot):
     entity.data.content
 
     snapshot.assert_match(content_data.content, f'further_reading_test.html')
+
+
+def test_further_reading_preprocessor_no_links(snapshot):
+    content_data = ContentData(
+        filename='a.md',
+        meta={},
+        content='content',
+    )
+    entity = DummyInclude(content_data)
+
+    FurtherReadingLinksPreprocessor.process_entity(entity)
+
+    entity.data.content
+
+    snapshot.assert_match(
+        content_data.content, f'further_reading_no_links_test.html'
+    )
+
+
+def test_further_reading_preprocessor_empty_content(snapshot):
+    content_data = ContentData(
+        filename='a.md',
+        meta={
+            'links': [
+                {'name': 'name.a', 'url': 'url.a'},
+                {'name': 'name.b', 'url': 'url.b'},
+                {'name': 'name.c', 'url': 'url.c'},
+            ]
+        },
+        content='',
+    )
+    entity = DummyInclude(content_data)
+
+    FurtherReadingLinksPreprocessor.process_entity(entity)
+
+    entity.data.content
+
+    # Shoudn't render anything
+    snapshot.assert_match(
+        content_data.content, f'further_reading_empty_content_test.html'
+    )
